ea33cacba3e90c8819d1f21dafc0412b9c69c533
guilherme <guilherme.tsilva03@gmail.com>
2023-06-19 13:52:43 -0400

    formulário de cadastro
    um formulário que manda automaticamente as informações para o numero de Whatsapp desejável

---
+++ .history/index_20230619133903.php
@@ -0,0 +1,146 @@
+<div id="banner-area" class="banner-area" style="background-color:#13564F; background:#D4F0ED">
+  <!-- para usar imagem... style="background-image:url(images/banner/banner1.jpg)" -->
+  <div class="banner-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="banner-heading">
+            <div>
+              <h1 class="north">TRABALHE CONOSCO</h1>
+              <p>*O sucesso passa através de pessoas*</p>
+            </div>
+            <div>
+              <img src="images/parceiro.png" alt="logo-parceiro">
+            </div>
+          </div>
+        </div><!-- Col end -->
+      </div><!-- Row end -->
+    </div><!-- Container end -->
+  </div><!-- Banner text end -->
+</div><!-- Banner area end -->
+
+<section id="main-container" class="main-container">
+  <div class="container">
+
+    <!--/ Title row end -->
+
+
+    <div class="gap-60"></div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <h3 class="column-title">
+          <center>FORMULÁRIO</center>
+          <center>Preencha o formulário para cadastro</center>
+        </h3>
+
+        <form>
+          <div class="error-container"></div>
+          <div class="row">
+            <div class="col-md-8">
+              <div class="form-group">
+                <label>Nome Completo / Razão Social <span style="color: red;">*</span></label>
+                <input class="form-control form-control-name" name="nome" id="nome" placeholder="Nome Completo/Razão Social" type="text" required>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>CPF / CNPJ</label>
+                <input class="form-control form-control-email" name="cpf-cnpj" id="cpf-cnpj" placeholder="CPF / CNPJ" type="text">
+              </div>
+            </div>
+
+            <div class="col-md-5">
+              <div class="form-group">
+                <label>Rua <span style="color: red;">*</span></label>
+                <input class="form-control form-control-email" name="rua" id="rua" placeholder="Rua" type="text" required>
+              </div>
+            </div>
+
+            <div class="col-md-2">
+              <div class="form-group">
+                <label>Nº <span style="color: red;">*</span></label>
+                <input class="form-control form-control-email" name="numero" id="numero" placeholder="Nº" type="text" required>
+              </div>
+            </div>
+
+            <div class="col-md-5">
+              <div class="form-group">
+                <label>Complemento</label>
+                <input class="form-control form-control-email" name="compl" id="compl" placeholder="Complemento" type="text">
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Bairro <span style="color: red;">*</span></label>
+                <input class="form-control form-control-email" name="bairro" id="bairro" placeholder="Bairro" type="text" required>
+              </div>
+            </div>
+
+            <div class="col-md-2">
+              <div class="form-group">
+                <label>Cep <span style="color: red;">*</span></label>
+                <input class="form-control cep-mask" name="cep" id="cep" placeholder="Cep " type="text" required>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Cidade <span style="color: red;">*</span></label>
+                <input class="form-control form-control-tel" name="cidade" id="cidade" placeholder="Cidade" type="text" required>
+              </div>
+            </div>
+
+            <div class="col-md-2">
+              <div class="form-group">
+                <label>UF <span style="color: red;">*</span></label>
+                <input class="form-control form-control-tel" name="uf" id="uf" placeholder="UF" type="text" required>
+              </div>
+            </div>
+
+
+            <div class="col-md-3">
+              <div class="form-group">
+                <label>Telefone Celular <span style="color: red;">*</span></label>
+                <input class="form-control form-control-tel" name="celular" id="celular" placeholder="Celular" type="tel" required>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="form-group">
+                <label>Telefone Residencial</label>
+                <input class="form-control form-control-tel" name="telefone" id="telefone" placeholder="Telefone Residencial" type="tel">
+              </div>
+            </div>
+
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>E-mail <span style="color: red;">*</span></label>
+                <input class="form-control form-control-tel" name="email" id="email" placeholder="E-mail" type="email" required>
+              </div>
+            </div>
+
+            <div class="col-md-2">
+              <div class="form-group">
+                <label>Nº de Registro CRC</label>
+                <input class="form-control form-control-tel" name="crc" id="crc" placeholder="CRC" type="text">
+              </div>
+            </div>
+
+          </div>
+
+          <div class="text-right"><br>
+            <button class="btn btn-primary solid blank" name="enviar" id="enviar" type="submit">Enviar Formulário</button>
+          </div>
+        </form>
+      </div>
+
+    </div><!-- Content row -->
+  </div><!-- Conatiner end -->
+
+  <script src=""></script>
+
+</section><!-- Main container end -->
