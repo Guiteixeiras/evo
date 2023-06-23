f346649c55704041c168d308f1c006d99f234695
guilherme <guilherme.tsilva03@gmail.com>
2023-06-23 09:13:41 -0400

    simple card

---
+++ cards/cards2/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="tudo">
+    <div class="arrumar">
+
+    <img class="mao"src="parceiro-emissao.png" alt="mão azul" style="padding:18px 17px !important;">
+
+    <h3 class="texto">textotextotextotextotextotexto</h3>
+
+    </div>
+
+    <body>
+    <div class="arrumar">
+
+    <img class="mao"src="parceiro-vendas.png" alt="mão azul">
+
+    <h3 class="texto">textotextotextotextotextotexto</h3>
+
+    </div>
+
+    <div class="arrumar">
+
+<img class="mao"src="portal-parceiro.png" alt="mão azul">
+
+<h3 class="texto">textotextotextotextotextotexto</h3>
+
+</div>
+
+
+    </div>
+</body>
+</html>
